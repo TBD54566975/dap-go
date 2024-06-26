d45842853d4b964dda221dec11b4e4585bb59d8c
Tom Daffurn <tdaffurn@tbd.email>
2024-06-26 11:41:01 +1000

    black box test KESMobileMoneyAddress

--- maddr/mobilemoney_test.go
+++ maddr/mobilemoney_test.go
@@ -1,6 +1,7 @@
-package maddr
+package maddr_test
 
 import (
+	"github.com/TBD54566975/dap-go/maddr"
 	"github.com/alecthomas/assert"
 	"reflect"
 	"testing"
@@ -8,13 +9,13 @@ import (
 
 func TestNewKESAddress(t *testing.T) {
 	tests := []struct {
-		maddr   *MoneyAddress
-		want    *KESMobileMoneyAddress
+		input   *maddr.MoneyAddress
+		want    *maddr.KESMobileMoneyAddress
 		wantErr bool
 	}{
 		{
-			maddr: newMoneyAddress(t, "urn:kes:momo:mpesa:254712345678"),
-			want: &KESMobileMoneyAddress{
+			input: newMoneyAddress(t, "urn:kes:momo:mpesa:254712345678"),
+			want: &maddr.KESMobileMoneyAddress{
 				MoneyAddress: *newMoneyAddress(t, "urn:kes:momo:mpesa:254712345678"),
 				Carrier:      "mpesa",
 				Phone:        "254712345678",
@@ -22,17 +23,17 @@ func TestNewKESAddress(t *testing.T) {
 			wantErr: false,
 		},
 		{
-			maddr:   newMoneyAddress(t, "urn:kes:momo:mpesa"),
+			input:   newMoneyAddress(t, "urn:kes:momo:mpesa"),
 			wantErr: true,
 		},
 		{
-			maddr:   newMoneyAddress(t, "urn:btc:addr:1LMcKyPmwebfygoeZP8E9jAMS2BcgH3Yip"),
+			input:   newMoneyAddress(t, "urn:btc:addr:1LMcKyPmwebfygoeZP8E9jAMS2BcgH3Yip"),
 			wantErr: true,
 		},
 	}
 	for _, tt := range tests {
-		t.Run(tt.maddr.URN.URN, func(t *testing.T) {
-			got, err := NewKESAddress(*tt.maddr)
+		t.Run(tt.input.URN.URN, func(t *testing.T) {
+			got, err := maddr.NewKESAddress(*tt.input)
 
 			if (err != nil) != tt.wantErr {
 				t.Errorf("NewKESAddress() error = %v, wantErr %v", err, tt.wantErr)
@@ -45,8 +46,8 @@ func TestNewKESAddress(t *testing.T) {
 	}
 }
 
-func newMoneyAddress(t *testing.T, maddr string) *MoneyAddress {
-	m, err := FromURN("didpay", maddr)
+func newMoneyAddress(t *testing.T, maddrURN string) *maddr.MoneyAddress {
+	m, err := maddr.FromURN("didpay", maddrURN)
 	assert.NoError(t, err)
 	return m
 }
